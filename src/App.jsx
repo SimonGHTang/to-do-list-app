7504840cb5ab5edb9a5ee44d28fc9ff1062ae4f5
Simon Tang <simon.gh.tang@gmail.com>
2023-11-10 14:48:10 +1100

    initial default list displays skeleton styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
+import DisplayTask from "./DisplayTask";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [taskList, setTaskList] = useState(() => createTodos()); //localStorage.getItem("taskList") | []
+	console.log(taskList);
+
+	function createTodos() {
+		return (
+			localStorage.getItem("taskList") || [
+				{ id: 1, completed: false, description: "default descrip" },
+			]
+		);
+	}
 
 	return (
-		<>
-		</>
+		<div className="list-container">
+			<h1>Task List</h1>
+			<span>{taskList.map(DisplayTask)}</span>
+		</div>
 	);
 }
 
